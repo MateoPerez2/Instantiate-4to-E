2a332e60031d4375d7281342d5010f7b9fadf241
MateoPerez2 <mateopaparella@hotmail.com>
2022-05-10 15:27:10 -0300

    Input field for text

---
+++ Assets/scirpts/dataImputManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dataImputManager : MonoBehaviour
+{
+    public Text txtUserName;
+
+    public void ShowGreetings()
+    {
+        Debug.Log("Hola " + txtUserName.text);
+    }
+    
+}
